6fda6e517a8cfa144ffddf545c0c64c00d454ebd
josemariasava <jo.m.sava@gmail.com>
2024-12-29 18:47:21 +0100

    Hangman game in python

---
+++ Hangman/Hangman_words.py
@@ -0,0 +1,7 @@
+# List of word 
+word_list = [
+    "Apple", "Dog", "Cat", "Bird", "Tree", "Sun", "Moon", "Book", "Chair", "Ball",
+    "Fish", "Star", "Cloud", "Bike", "Door", "Hand", "Car", "Lion", "Shoe", "Cup",
+    "Frog", "Duck", "Rain", "Leaf", "King", "Queen", "Bell", "Cup", "Ship", "Home",
+    "Wall", "Snow", "Boat", "Plate", "Cheese", "Heart", "Wolf", "Mouse", "Rocket", "Cup"
+]
